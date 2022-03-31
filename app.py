fb6a97d8efb660ac0e86b10884088c73ce64f22e
cshelton7 <clshelton7@gmail.com>
2022-03-31 02:14:46 -0400

    testing deployment in main

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def home():
 
 if __name__ == "__main__":
     app.run(
-      #  host=os.getenv("IP", "0.0.0.0"), 
-      #  port=int(os.getenv("PORT", 8080)), 
+        host=os.getenv("IP", "0.0.0.0"), 
+        port=int(os.getenv("PORT", 8080)), 
         debug=True
     )
