2b325702334d03766ffa64a07719ab02da53842d
cshelton7 <clshelton7@gmail.com>
2022-03-31 04:15:34 -0400

    client id env variable for security

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
+import os
 from flask import Flask, render_template, redirect, flash, request
 from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+# Code from project milestones
+
+# point to heroku database
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+# remove a warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# a way to replace postgres with postgresql & it works for some reason
+if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ].replace("postgres://", "postgresql://")
+
+# 
 
 @app.route("/")
 def login():
@@ -11,6 +28,7 @@ def login():
     """
     return render_template(
         "login.html",
+        client_id = os.getenv("GSCLIENTID")
     )
 
 @app.route("/home")
