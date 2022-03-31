5e6bcb4eb187c61772b5c333f18d843c0650f74e
cshelton7 <clshelton7@gmail.com>
2022-03-31 03:08:01 -0400

    testing out Google API code

--- app.py
+++ app.py
@@ -21,3 +21,10 @@ def home():
     return render_template(
         "home.html",
     )
+
+if __name__ == "__main__":
+    app.run(
+       # host=os.getenv("IP", "0.0.0.0"), 
+       # port=int(os.getenv("PORT", 8080)), 
+        debug=True
+    )
