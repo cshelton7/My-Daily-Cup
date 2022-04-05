73d2a575fb86f58cae2ae77307d6ee5ddb82bd25
cshelton7 <clshelton7@gmail.com>
2022-04-05 19:12:26 -0400

    remove tester code

--- models.py
+++ models.py
@@ -1,11 +1,8 @@
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
-# uncomment after testing
-# db = SQLAlchemy()
+db = SQLAlchemy()
 
-# debug code for db
-from app import db
 
 
 class User(db.Model, UserMixin):
