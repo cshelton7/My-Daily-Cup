7064428b48df999c8c64d9e85528971f782cf928
Isaiah Isaac Bellanton <ibell8@Isaiahs-MacBook-Air.local>
2022-04-06 09:24:53 -0400

    This is adding the datase functions file, so it gets rid of the error

---
+++ database_functions.py
@@ -0,0 +1,11 @@
+from models import db, Joes, Entry
+
+def get_entries(user_id):
+    entries = Entry.query.filter_by(user=user_id).all()
+    return entries
+
+def deleteEntry(entry_id):
+    entry = Entry.query.filter_by(id=entry_id)
+    if entry:
+        db.session.delete(entry)
+        db.session.commit()
