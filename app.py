2919eb9d4d2f790e53fe22a43be2bc664d19de33
Constance Shelton <77510812+cshelton7@users.noreply.github.com>
2022-04-07 02:43:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,42 +8,42 @@ from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 # Create app, configure db
-    app = Flask(__name__)
-    app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
-    app.secret_key = os.getenv("SECRET")
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
-        app.config["SQLALCHEMY_DATABASE_URI"] = app.config[
-            "SQLALCHEMY_DATABASE_URI"
-        ].replace("postgres://", "postgresql://")
+app = Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.secret_key = os.getenv("SECRET")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+if app.config["SQLALCHEMY_DATABASE_URI"].startswith("postgres://"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ].replace("postgres://", "postgresql://")
 
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
 
-    app.register_blueprint(auth_blueprint)
+app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
+# blueprint for non-auth parts of app
+from .main import main as main_blueprint
 
-    app.register_blueprint(main_blueprint)
+app.register_blueprint(main_blueprint)
 
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.init_app(app)
 
-    from .models import db, Joes
+from .models import db, Joes
 
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        return Joes.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return Joes.query.get(int(user_id))
 
-    if __name__ == "__main__":
-        app.run(
-            host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
-        )
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
+    )
