961a23dc6d52b64f0cf7e7d91ea1599194b4ca6d
cshelton7 <clshelton7@gmail.com>
2022-04-07 02:34:47 -0400

    moving files out

--- project/fun_fact.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def fun_fact():
-    """Displays a random fun fact"""
-    responses = requests.get("https://api.aakhilv.me/fun/facts")
-    responses_json = responses.json()
-    return responses_json[0]
