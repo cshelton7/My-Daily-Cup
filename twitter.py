e7e5c76c9d840a6a0b180b9c78c85899bb1de6e8
Isaiah Isaac Bellanton <ibell8@isaiahs-air.home>
2022-04-08 13:24:57 -0400

    Added a Unit test for the Twitter API to make sure we are getting the responses that we are looking for from the API in use. I also edited twitter.py to make sure that it was possible to run unit tests on them

--- twitter.py
+++ twitter.py
@@ -1,9 +1,7 @@
 # import the module
 import os
 import tweepy
-
 from dotenv import find_dotenv, load_dotenv
-
 load_dotenv(find_dotenv())
 
 
@@ -11,26 +9,28 @@ def get_trends():
     # assign the values accordingly
     consumer_key = os.getenv("TWITTER_KEY")
     consumer_secret = os.getenv("TWITTER_SECRET")
-
     # authorization of consumer key and consumer secret
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
-    # getting authorization for our keys
-    api = tweepy.API(auth)
 
     # WOEID of the United States
     woeid = 23424977
 
-    # fetching the trends
-    trends = api.get_place_trends(id=woeid)
-
+    # getting authorization for our keys, then finding the trending topics
+    trends = tweepy.API(auth).get_place_trends(id=woeid)
+    
+    trends = trends[0]['trends']
+    
+    
     # This will find the top 5 trending topics
     trending_topics = []
     i = 0
-    for value in trends:
-        for trend in value["trends"]:
-            trending_topics.append(trend["name"])
-            i += 1
-            if i >= 5:
-                break
+    for trend in trends:
+        trending_topics.append(trend['name'])
+        i +=1
+        if(i==5):
+            break
     return trending_topics
+    
+
+if __name__ == "__main__":
+    get_trends()
